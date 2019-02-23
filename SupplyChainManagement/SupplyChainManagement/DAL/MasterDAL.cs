c7261a952e882f72f7d2a273446319fb9eb684ec
Jimmy Mathew <jimmy.connect@gmail.com>
2019-02-23 12:51:44 +0530

    DAL layer
    Entity framework, Models, sql backup

---
+++ SupplyChainManagement/SupplyChainManagement/DAL/MasterDAL.cs
@@ -0,0 +1,87 @@
+﻿using SupplyChainManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupplyChainManagement.DAL
+{
+    public class MasterDAL
+    {
+        scmEntities entities = new scmEntities();
+        #region User Details
+        public List<UserDetails> ReadUserDetails()
+        {
+            return entities.users.Select(x => new UserDetails
+
+            {
+                id = x.id,
+                name = x.name,
+                usename = x.usename,
+                password = x.password,
+                role = x.role
+            }).OrderByDescending(x => x.id).ToList();
+        }
+        public Response SaveUserDetails(UserDetails obj)
+        {
+            List<UserDetails> userList = new List<UserDetails>();
+            var IsExist = entities.users.Where(x => x.usename == obj.usename).ToList();
+            if (IsExist.Count == 0)
+            {
+                user itemObj = new user();
+                itemObj.id = obj.id;
+                itemObj.name = obj.name;
+                itemObj.name = obj.name;
+                itemObj.password = obj.password;
+                itemObj.role = obj.role;
+                itemObj.createdBy = "Admin";
+                itemObj.createdOn = DateTime.Now;
+                itemObj.updatedBy = "Admin";
+                itemObj.updatedOn = DateTime.Now;
+                entities.users.Add(itemObj);
+
+                entities.SaveChanges();
+                return new Response { IsSuccess = true, Message = "User details added successfully" };
+            }
+            else
+                return new Response { IsSuccess = false, Message = "User already exists" };
+
+        }
+        public Response UpdateUserDetails(UserDetails obj)
+        {
+            List<UserDetails> userList = new List<UserDetails>();
+            var IsExist = entities.users.Where(x => x.id == obj.id).ToList();
+            if (IsExist.Count != 0)
+            {
+                var itemObj = entities.users.Where(x => x.id == obj.id).FirstOrDefault();
+                itemObj.id = obj.id;
+                itemObj.name = obj.name;
+                itemObj.usename = obj.usename;
+                itemObj.password = obj.password;
+                itemObj.role = obj.role;
+                itemObj.createdBy = "Admin";
+                itemObj.createdOn = DateTime.Now;
+                itemObj.updatedBy = "Admin";
+                itemObj.updatedOn = DateTime.Now;
+                entities.SaveChanges();
+                return new Response { IsSuccess = true, Message = "User details updated successfully " };
+            }
+            else
+                return new Response { IsSuccess = false, Message = "User already exists" };
+
+        }
+        public Response DeleteUserDetails(int id)
+        {
+            var itemObj = entities.users.Where(x => x.id == id).FirstOrDefault();
+            if (itemObj != null)
+            {
+                entities.users.Remove(itemObj);
+                entities.SaveChanges();
+                return new Response { IsSuccess = false, Message = "User details deleted successfully" };
+            }
+            else
+                return new Response { IsSuccess = false, Message = "Data deletion error. Contact Administrator" };
+        }
+        #endregion
+    }
+}
---
+++ SupplyChainManagement/SupplyChainManagement/Models/UserDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupplyChainManagement.Models
+{
+    public class UserDetails
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string usename { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+        public string createdBy { get; set; }
+        public Nullable<System.DateTime> createdOn { get; set; }
+        public string updatedBy { get; set; }
+        public Nullable<System.DateTime> updatedOn { get; set; }
+    }
+}
